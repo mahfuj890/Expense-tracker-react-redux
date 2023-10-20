ea6623b357aaacb0136fdfd8af9158faccb3136e
mahfuj890 <mahfujali490@gmail.com>
2023-10-20 12:29:59 +0600

    create slice and api

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import transactionSlice from "../features/transaction/transactionSlice";
 
 export const store = configureStore({
-    reducer: {},
+  reducer: { transaction: transactionSlice },
 });
