3e80de860f9db78ee60340284b68522be6935da8
mahfuj890 <mahfujali490@gmail.com>
2023-10-21 13:23:41 +0600

    create and list

--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -1,19 +1,21 @@
 import deleteImage from "../../assets/images/delete.svg";
 import editImage from "../../assets/images/edit.svg";
 
-export default function Transaction() {
-    return (
-        <li className="transaction income">
-            <p>Earned this month</p>
-            <div className="right">
-                <p>৳ 100</p>
-                <button className="link">
-                    <img alt="Edit" className="icon" src={editImage} />
-                </button>
-                <button className="link">
-                    <img alt="Delete" className="icon" src={deleteImage} />
-                </button>
-            </div>
-        </li>
-    );
+export default function Transaction({ transaction }) {
+  const { name, amount, type } = transaction || {};
+
+  return (
+    <li className={`transaction ${type}`}>
+      <p>{name}</p>
+      <div className="right">
+        <p>৳ {amount}</p>
+        <button className="link">
+          <img alt="Edit" className="icon" src={editImage} />
+        </button>
+        <button className="link">
+          <img alt="Delete" className="icon" src={deleteImage} />
+        </button>
+      </div>
+    </li>
+  );
 }
--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,15 +1,34 @@
 import Transaction from "./Transaction";
+import { useSelector } from "react-redux";
 
 export default function Transactions() {
-    return (
-        <>
-            <p className="second_heading">Your Transactions:</p>
+  const { transactions, isLoading, isError } = useSelector(
+    (state) => state.transaction
+  );
 
-            <div className="conatiner_of_list_of_transactions">
-                <ul>
-                    <Transaction />
-                </ul>
-            </div>
-        </>
-    );
+  // decide what to render
+  let content = null;
+  if (isLoading) content = <p>Loading...</p>;
+
+  if (!isLoading && isError)
+    content = <p className="error">There was an error occured</p>;
+
+  if (!isLoading && !isError && transactions?.length > 0) {
+    content = transactions.map((transaction) => (
+      <Transaction key={transaction.id} transaction={transaction} />
+    ));
+  }
+
+  if (!isLoading && !isError && transactions?.length === 0) {
+    content = <p>No transactions found!</p>;
+  }
+  return (
+    <>
+      <p className="second_heading">Your Transactions:</p>
+
+      <div className="conatiner_of_list_of_transactions">
+        <ul>{content}</ul>
+      </div>
+    </>
+  );
 }
